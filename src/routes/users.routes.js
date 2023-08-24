650b9cadd1764bf270fc9873b32c0a1fedd9643c
Victor Raposo <thevhup@gmail.com>
2023-08-24 16:33:40 -0300

    add put ( update ) method at userscontrollers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,5 +6,7 @@ const usersRoutes = Router()
 const usersController = new UsersController()
 
 usersRoutes.post("/", usersController.create)
+usersRoutes.put("/:id", usersController.update)
+
 
 module.exports = usersRoutes
