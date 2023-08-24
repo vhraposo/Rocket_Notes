5178fd42a8825cbe8cd889148bcc0676e02fb413
Victor Raposo <thevhup@gmail.com>
2023-08-24 16:59:33 -0300

    add update password at users controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 
 const AppError = require("../utils/AppError")
 
@@ -38,7 +38,7 @@ class UsersController {
   }
 
   async update(request, response){
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
@@ -56,13 +56,28 @@ class UsersController {
     user.name = name
     user.email = email 
 
+    if( password && !old_password ){
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
+    }
+
+    if(password && old_password){
+      const checkOldPassword = await compare(old_password, user.password)
+      
+      if(!checkOldPassword){
+        throw new AppError("A senha antiga não confere.")
+      }
+
+      user.password = await hash(password, 8)
+    }
+
     await database.run(`
       UPDATE users SET
       name = ?,
       email = ?,
+      password = ?,
       updated_at = ? 
       WHERE id = ?`,
-      [ user.name, user.email, new Date(), id] 
+      [ user.name, user.email, user.password, new Date(), id] 
       )
     return response.json()
   }
