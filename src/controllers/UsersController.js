bc853f6eafc01c4e759708e683c1743868ebf24b
Victor Raposo <thevhup@gmail.com>
2023-08-24 16:17:36 -0300

    add bcryptjs at user password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,35 +1,40 @@
+const { hash } = require("bcryptjs")
+
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
 
-
-
 class UsersController {
   /**
    *  index - GET para listar vários registros.
-   * show - GET para exibir um registro específico. 
+   * show - GET para exibir um registro específico.
    * create - POST para criar um registro.
    * update- PUT para atualizar um registro.
    * delete - DELETE para remover um registro.
-   * 
+   *
    */
 
-  async create(request, response){
+  async create(request, response) {
     const { name, email, password } = request.body
 
     const database = await sqliteConnection()
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const checkUserExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    )
 
-    if(checkUserExists){
+    if (checkUserExists) {
       throw new AppError("Este e-mail já está em uso.")
     }
 
-    await database.run("INSERT INTO users (name, email, password) VALUES ( ?, ?, ? )",
-    [ name, email, password ]
+    const hashPassword = await hash(password, 8)
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES ( ?, ?, ? )",
+      [name, email, hashPassword]
     )
 
     return response.status(201).json()
-
   }
 }
 
