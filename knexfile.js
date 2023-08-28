ea47a857f552c9cf1ffc7cdf90b4c1cdcd134729
Victor Raposo <thevhup@gmail.com>
2023-08-28 17:33:04 -0300

    add migrates

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,9 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
     useNullAsDefault: true
   }
 };
