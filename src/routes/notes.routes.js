b77574d566242fe868eaa5c75551e2e8efe1f144
Victor Raposo <thevhup@gmail.com>
2023-08-30 18:24:23 -0300

    add GET method at NotesController

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,6 +6,8 @@ const notesRoutes = Router()
 const notesController = new NotesController()
 
 notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.get("/:id", notesController.show)
+
 
 
 
