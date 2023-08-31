30cb8776df534704e6a8615fce2174098e99f1e5
Victor Raposo <thevhup@gmail.com>
2023-08-31 10:30:14 -0300

    add delete method at notes controller.js

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,6 +7,8 @@ const notesController = new NotesController()
 
 notesRoutes.post("/:user_id", notesController.create)
 notesRoutes.get("/:id", notesController.show)
+notesRoutes.delete("/:id", notesController.delete)
+
 
 
 
