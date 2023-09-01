2686fdc6e9016a136729261e3531fd00ecc3017b
Victor Raposo <thevhup@gmail.com>
2023-09-01 09:01:40 -0300

    add tagscontroller - method get Index

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,15 @@
+const { response } = require("express")
+const knex = require("../database/knex")
+
+class TagsController{
+  async index(request, response){
+    const { user_id } = request.params
+
+    const tags = await knex("tags")
+    .where({ user_id })
+
+    return response.json(tags)
+  }
+}
+
+module.exports = TagsController
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,15 @@ const { Router } = require("express")
 
 const usersRouter = require("./users.routes")
 const notesRouter = require("./notes.routes")
+const tagsRouter = require("./tags.routes")
+
 
 
 const routes = Router()
 routes.use("/users", usersRouter)
 routes.use("/notes", notesRouter)
+routes.use("/tags", tagsRouter)
+
 
 
 
