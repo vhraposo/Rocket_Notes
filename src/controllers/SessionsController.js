b25aadd340b5f51e374da44aafd8276c3c8bb663
vhraposo <thevhup@gmail.com>
2023-10-05 10:15:57 -0300

    create validation if user password matched  at sessions controller

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require('../database/knex')
 const AppError = require('../utils/AppError')
+const { compare } = require("bcryptjs")
 
 class SessionsController{
     async create(request, response){
@@ -10,6 +11,13 @@ class SessionsController{
        if(!user){
         throw new AppError('Email e/ou senha incorreta', 401)
        }
+
+       const passwordMatched = await compare(password, user.password)
+
+       if(!passwordMatched){
+            throw new AppError("Email e/ou senha incorreta", 401)
+       }
+
         return response.json({ user })
     }
 }
