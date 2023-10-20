943c4651cd15d632094568c78746e878c4f2c873
vhraposo <thevhup@gmail.com>
2023-10-20 13:29:32 -0300

    refactor: edit response remove noteWithTags at notes controller

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -75,6 +75,7 @@ class NotesController {
       .whereLike("notes.title",`%${title}%` )
       .whereIn("name", filterTags)
       .innerJoin("notes", "notes.id", "tags.note_id")
+      .groupBy("notes.id")
       .orderBy("notes.title")
 
     }else{
@@ -94,7 +95,7 @@ class NotesController {
       }
     })
 
-    return response.json({ notesWithTags })
+    return response.json( notesWithTags )
   }
 }
 
